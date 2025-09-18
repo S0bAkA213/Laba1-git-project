747978cb705698e050cc8faa8ccd268ac61c5d9b
chiefkeef <rinatcrazy2006@gmail.com>
2025-09-18 16:02:08 +0500

    Второй коммит, есть два принта (имя фамилия,группа)

--- Program_example.py
+++
@@ -1,4 +0,0 @@
-def main():
-    print('my first git program')
-if __name__ == '__main__':
-    main()
---
+++ Program_example_Ибрагимов_Ринат.py
@@ -0,0 +1,5 @@
+def main():
+    print('Ибрагимов Ринат')
+    print('ИВТ-ИВС-202Б')
+if __name__ == '__main__':
+    main()
