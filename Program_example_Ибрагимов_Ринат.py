70f6cc26aa496b39e55338e0d1e46a6f00c2095c
S0bAkA213 <133011538+S0bAkA213@users.noreply.github.com>
2025-09-18 16:43:13 +0500

    4 коммит сделанный из гитхаба

--- Program_example_Ибрагимов_Ринат.py
+++ Program_example_Ибрагимов_Ринат.py
@@ -2,5 +2,6 @@ def main():
     print('Ибрагимов Ринат')
     print('ИВТ-ИВС-202Б')
     print('18.09.2025')
+    print('hustlers don't stop, they keep going')
 if __name__ == '__main__':
     main()
